107a66c50dfeeb200625cd8afb70bf7d9cec240b
Ian Tait <thetaiter@gmail.com>
2016-10-13 00:50:28 -0400

    Updated Jenkins data for nodes

--- bin/run_jenkins.sh
+++ bin/run_jenkins.sh
@@ -5,7 +5,7 @@ ROOT="$(dirname "${BIN}")"
 docker service create \
 	--network frontend \
 	--endpoint-mode vip \
-	--mount "type=bind,source=${ROOT}/data/jenkins,target=/var/jenkins_home" \
+	--mount "type=bind,source=${ROOT}/data/jenkins/home,target=/var/jenkins_home" \
 	--replicas 1 \
 	--name jenkins \
 	jenkins:2.19.1-alpine
