50a6bceb49c6298bd3ba4c02d356a8facc8304d2
Ian Tait <thetaiter@gmail.com>
2016-10-26 13:45:21 -0400

    Added timezone info to Jenkins run script

--- bin/run_jenkins.sh
+++ bin/run_jenkins.sh
@@ -6,9 +6,11 @@ docker service create \
 	--network frontend \
 	--endpoint-mode vip \
 	--mount "type=bind,source=${ROOT}/data/jenkins/home,target=/var/jenkins_home" \
+	--mount "type=bind,source=/etc/localtime,target=/etc/localtime,readonly=true" \
 	--replicas 1 \
 	--restart-condition any \
 	--restart-delay 5s \
 	--constraint engine.labels.role==manager \
+	--env "JAVA_OPTS=-Duser.timezone=America/Central" \
 	--name jenkins \
 	jenkins:2.19.1-alpine
